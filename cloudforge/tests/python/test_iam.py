b72b027cde30b61eb82821ce9e35cceb6998e8f6
Bikash Thakur <xbikashthakur@gmail.com>
2025-10-18 14:37:38 +0000

    fixed pre-commit errors

--- cloudforge/tests/python/test_iam.py
+++ cloudforge/tests/python/test_iam.py
@@ -1,11 +1,12 @@
 import boto3
-import pytest
-from moto import mock_aws # Changed from mock_iam for newer moto versions
+from moto import mock_aws  # Changed from mock_iam for newer moto versions
 
-# The @mock_aws decorator intercepts any boto3 calls made within this function
-# and redirects them to the in-memory mock AWS environment instead of the real one.
+
+# The @mock_aws decorator intercepts any boto3 calls
+# made within this function and redirects them to the
+# in-memory mock AWS environment instead of the real one.
 @mock_aws
-def test_iam_user_creation():
+def test_iam_user_creation() -> None:
     """
     Tests that an IAM user can be created successfully.
     """
@@ -25,20 +26,23 @@ def test_iam_user_creation():
     # We expect the created user's name to match our test username.
     assert users[0]["UserName"] == test_username
 
+
 @mock_aws
-def test_iam_policy_creation():
+def test_iam_policy_creation() -> None:
     """
     Tests that a simple IAM policy can be created.
     """
     # 1. Setup
     iam_client = boto3.client("iam", region_name="us-east-1")
     test_policy_name = "TestPolicy"
-    policy_document = '{"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}]}'
+    policy_document = (
+        '{"Version": "2012-10-17", '
+        '"Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}]}'
+    )
 
     # 2. Action
     iam_client.create_policy(
-        PolicyName=test_policy_name,
-        PolicyDocument=policy_document
+        PolicyName=test_policy_name, PolicyDocument=policy_document
     )
 
     # 3. Assertion
